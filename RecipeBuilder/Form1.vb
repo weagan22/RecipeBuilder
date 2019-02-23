2b16c35f6ba8ff4affce4de59284cd9d2a1898f7
weagan22 <weagan22@gmail.com>
2019-02-23 09:28:45 -0800

    Added features to gui.

--- RecipeBuilder/Form1.vb
+++ RecipeBuilder/Form1.vb
@@ -1,5 +1,9 @@
-﻿Public Class Form1
+﻿Public Class MainForm
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Chart1.Series(0).ChartType = DataVisualization.Charting.SeriesChartType.Line
     End Sub
 End Class
--- RecipeBuilder/My Project/Application.Designer.vb
+++ RecipeBuilder/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.RecipeBuilder.Form1
+            Me.MainForm = Global.RecipeBuilder.MainForm
         End Sub
     End Class
 End Namespace
